187670700e35ee83610432a4a82f2a0dc70f17d8
Feng Xia <feng_xia41103@hotmail.com>
2017-02-05 08:53:45 -0500

    Update emacs init.el to allow save hook for web-beautify in js and js-jsx mode. js2 mode was already working.

--- content/downloads/init.el
+++ content/downloads/init.el
@@ -200,6 +200,35 @@
 (require 'js-auto-beautify)
 
 (add-hook 'js2-mode-hook 'js-auto-beautify-mode)
+(eval-after-load 'js2-mode
+  '(add-hook 'js2-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+
+(eval-after-load 'js-jsx-mode
+  (add-hook 'js-jsx-mode-hook '
+            (lambda ()
+              (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+
+(eval-after-load 'js-mode
+  '(add-hook 'js-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+
+(eval-after-load 'json-mode
+  '(add-hook 'json-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+
+(eval-after-load 'sgml-mode
+  '(add-hook 'html-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-html-buffer t t))))
+
+(eval-after-load 'css-mode
+  '(add-hook 'css-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-css-buffer t t))))
 
 ;;----------------------------------------------------------------------------
 ;; py-autopep8
@@ -328,7 +357,7 @@ future."
 
 ;; Great font on the mac
 ;; (set-face-attribute 'default nil :family "Bitstream Vera Sans Mono" :height 100)
-(set-face-attribute 'default nil :family "Bitstream Vera Sans Mono" :height 130)
+;;(set-face-attribute 'default nil :family "Bitstream Vera Sans Mono" :height 130)
 
 ;; Is this now removed by default?
 (menu-bar-mode 1)

c3de506e5037679944d042cb5c5ea9fef697fbcd
Feng Xia <feng_xia41103@hotmail.com>
2017-02-27 21:24:16 -0500

    Test buildbot.

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,11 +15,11 @@ TIMEZONE = 'America/New_York'
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+#FEED_ALL_ATOM = None
+#CATEGORY_FEED_ATOM = None
+#TRANSLATION_FEED_ATOM = None
+#AUTHOR_FEED_ATOM = None
+#AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
@@ -41,6 +41,7 @@ EMAIL_ADDRESS = 'feng_xia41103@hotmail.com'
 GITHUB_ADDRESS = 'http://github.com/fengxia41103'
 LINKEDIN_ADDRESS = 'https://www.linkedin.com/in/fengxia41103'
 
-LOAD_CONTENT_CACHE = False  # for development use
+LOAD_CONTENT_CACHE = True  # for development use
+
 #SUMMARY_MAX_LENGTH = 300
 IGNORE_FILES = ["README.*", "readme.*", "Readme.*"]

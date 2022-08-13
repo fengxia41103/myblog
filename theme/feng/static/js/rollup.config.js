ee6290ba0299e0e69256af5eb13f0d2b2fb8378c
Feng Xia <feng.xia@lucidum.io>
2022-08-13 12:56:59 -0400

    feat: New rollup config, quote jsx

--- theme/feng/static/js/rollup.config.js
+++ theme/feng/static/js/rollup.config.js
@@ -5,9 +5,9 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import replace from "@rollup/plugin-replace";
 export default {
-  input: "brother.jsx",
+  input: "index.jsx",
   output: {
-    file: "../../../../output/theme/js/brother.js",
+    file: "../../../../output/theme/js/bundle.js",
     format: "iife",
     sourcemap: true,
   },
